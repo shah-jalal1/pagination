13380d96d33059d21d2b95af3dff4706f774676a
shah-jalal1 <shah.jalal0894@gmail.com>
2022-03-18 18:37:52 +0600

    home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -14,7 +14,7 @@ import {
 } from "@mui/material";
 import { read } from 'fs';
 import React, { Component, useEffect, useState } from 'react';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 
 
@@ -44,121 +44,225 @@ const Home: React.FC = () => {
 
     const history = useHistory();
 
-    const [page, setPage] = useState<number>(0);
-
-    const [posts, setPosts] = useState<InitPost[]>([]);
-
-    const [totalElements, setTotalElements] = useState<number>(0);
-
+    const [pageNo, setPageNo] = useState<number>(0);
     const [localPage, setLocalPage] = useState<number>(1);
-
-    const rowsPerPage: number = 20;
+    const [totalElements, setTotalElements] = useState<number>(0);
+    const [posts, setPosts] = useState<InitPost[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const rowsPerPage = 20;
 
     useEffect(() => {
-
         const interval = setInterval(() => {
-            setPage(_page => _page + 1);
+            setPageNo((_page) => _page + 1);
         }, 10000);
 
         return () => clearInterval(interval);
     }, []);
 
     useEffect(() => {
-
         getPosts();
-
-    }, [page])
+    }, [pageNo]);
 
     const getPosts = async () => {
         try {
-            const res = await fetch(`https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${page}`);
+            setLoading(true);
+            const res = await fetch(`https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${pageNo}`);
             const data = await res.json();
             const _posts = [...posts, ...data?.hits];
             setPosts(_posts);
             setTotalElements(_posts.length);
-
-
+            setLoading(false);
+        } catch (e) {
+            setLoading(false);
+            console.log(e);
         }
-        catch (error) {
+    };
 
-        }
-    }
-    const getDetails = (post: InitPost) => {
-        history.push({
-            pathname: '/details',
-            state: post
-        })
-    }
-
-    const handleChangePage = async(event: unknown, newPage: number) => {
+    const handlePageChange = (e: unknown, newPage: number) => {
         setLocalPage(newPage);
-    }
+    };
+
+    const handleDetails = (post: InitPost) => {
+        history.push("/details", post);
+    };
 
     return (
-        <>
-         <h2>Post List</h2>
-
-<Container>
-    <Paper>
-        <TableContainer>
-        <Table stickyHeader aria-label="sticky-table">
-            <TableHead>
-            <TableRow>
-                        {
-                            columns.map(column =>
-                                <TableCell
-                                    key={column.id}
-                                    align={column.align}
-                                    style={{minWidth: column.minWidth}}
-                                >
-                                    {column.label}
-                                </TableCell>
-                            )
-                        }
-                    </TableRow>
-            </TableHead>
-
-            <TableBody>
-                {
-                    posts
-                    .slice((localPage -1) * rowsPerPage, (localPage-1) * rowsPerPage + rowsPerPage)
-                    .map((row, index) => {
-                        return(
-                            <TableRow 
-                            key={index}
-                            onClick={()=> getDetails(row)}>
-                                {
-                                    columns.map(column => {
-                                        const value = row[column.id];
-                                        return(
-                                            <TableCell
-                                            key={column.id}>
-                                                {value}
-                                            </TableCell>
-                                        )
-                                    })
-                                }
-
+        <Container data-testid="home">
+            <h3 style={{ textAlign: "center" }} data-testid="text">Post Table</h3>
+            {loading ? (
+                <Box style={{ textAlign: "center" }} data-testid="loading">
+                    <CircularProgress size={25} />
+                    Loading new Post Data...
+                </Box>
+            ) : (
+                <TableContainer component={Paper}>
+                    <Table style={{ minWidth: 650 }} aria-label="simple table"  data-testid="table">
+                        <TableHead>
+                            <TableRow>
+                                {columns.map((column, i) => (
+                                    <TableCell
+                                        key={i}
+                                        align={column.align}
+                                        style={{ minWidth: column.minWidth }}
+                                    >
+                                        {column.id}
+                                    </TableCell>
+                                ))}
                             </TableRow>
-                        )
-                    })
-                }
-            </TableBody>
-        </Table>
-        </TableContainer>
-        
-         <Pagination
-              
+                        </TableHead>
+                        <TableBody>
+                            {posts
+                                .slice(
+                                    rowsPerPage * (localPage - 1),
+                                    rowsPerPage * (localPage - 1) + rowsPerPage
+                                )
+                                .map((row) => (
+                                    <TableRow
+                                        key={row.title}
+                                        onClick={() => handleDetails(row)}
+                                        style={{ cursor: "pointer" }}
+                                    >
+                                        {columns.map((column) => (
+                                            <TableCell
+                                                key={column.id}
+                                                align={column.align}
+                                                style={{ minWidth: column.minWidth }}
+                                            >
+                                                {row[column.id]}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            )}
+            <Pagination
                 count={totalElements / rowsPerPage}
                 page={localPage}
-                onChange={handleChangePage}
-            
+                onChange={handlePageChange}
             />
-        
-    </Paper>
-</Container>
-        </>
+        </Container>
     );
 };
+
+//     const history = useHistory();
+
+//     const [page, setPage] = useState<number>(0);
+
+//     const [posts, setPosts] = useState<InitPost[]>([]);
+
+//     const [totalElements, setTotalElements] = useState<number>(0);
+
+//     const [localPage, setLocalPage] = useState<number>(1);
+
+//     const rowsPerPage: number = 20;
+
+//     useEffect(() => {
+
+//         const interval = setInterval(() => {
+//             setPage(_page => _page + 1);
+//         }, 10000);
+
+//         return () => clearInterval(interval);
+//     }, []);
+
+//     useEffect(() => {
+
+//         getPosts();
+
+//     }, [page])
+
+//     const getPosts = async () => {
+//         try {
+//             const res = await fetch(`https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${page}`);
+//             const data = await res.json();
+//             const _posts = [...posts, ...data?.hits];
+//             setPosts(_posts);
+//             setTotalElements(_posts.length);
+
+
+//         }
+//         catch (error) {
+
+//         }
+//     }
+//     const getDetails = (post: InitPost) => {
+//         history.push({
+//             pathname: '/details',
+//             state: post
+//         })
+//     }
+
+//     const handleChangePage = async(event: unknown, newPage: number) => {
+//         setLocalPage(newPage);
+//     }
+
+//     return (
+//         <>
+//          <h2>Post List</h2>
+
+// <Container>
+//     <Paper>
+//         <TableContainer>
+//         <Table stickyHeader aria-label="sticky-table">
+//             <TableHead>
+//             <TableRow>
+//                         {
+//                             columns.map(column =>
+//                                 <TableCell
+//                                     key={column.id}
+//                                     align={column.align}
+//                                     style={{minWidth: column.minWidth}}
+//                                 >
+//                                     {column.label}
+//                                 </TableCell>
+//                             )
+//                         }
+//                     </TableRow>
+//             </TableHead>
+
+//             <TableBody>
+//                 {
+//                     posts
+//                     .slice((localPage -1) * rowsPerPage, (localPage-1) * rowsPerPage + rowsPerPage)
+//                     .map((row, index) => {
+//                         return(
+//                             <TableRow 
+//                             key={index}
+//                             onClick={()=> getDetails(row)}>
+//                                 {
+//                                     columns.map(column => {
+//                                         const value = row[column.id];
+//                                         return(
+//                                             <TableCell
+//                                             key={column.id}>
+//                                                 {value}
+//                                             </TableCell>
+//                                         )
+//                                     })
+//                                 }
+
+//                             </TableRow>
+//                         )
+//                     })
+//                 }
+//             </TableBody>
+//         </Table>
+//         </TableContainer>
+
+//          <Pagination
+
+//                 count={totalElements / rowsPerPage}
+//                 page={localPage}
+//                 onChange={handleChangePage}
+
+//             />
+
+//     </Paper>
+// </Container>
+//         </>
+//     );
+// };
 
 export default Home;
